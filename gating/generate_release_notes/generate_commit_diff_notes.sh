f40f1c968dcf1dcd9ce63470c8ef8c74e6634876
Kevin Carter <kevin.carter@rackspace.com>
2017-12-12 15:56:53 -0600

    [RO-3244] Link a role requirement file for rpc-differ
    The rpc-differ application expects to find the
    "ansible-role-requiremnts.yml" file in the root path of the repo. This
    change make diff generation tooling link a product requirements file to
    expected path when executed. Because this change has the potential to
    create a modified git tree, the "ansible-role-requirements.yml" has been
    added to .gitignore.
    
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- gating/generate_release_notes/generate_commit_diff_notes.sh
+++ gating/generate_release_notes/generate_commit_diff_notes.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
+export BASE_DIR="$(readlink -f $(dirname ${0}))"
+source "scripts/functions.sh"
+ln -sf "ansible-role-${RPC_PRODUCT_RELEASE}-requirements.yml" "ansible-role-requirements.yml"
 
 rpc-differ --debug -r "$REPO_URL" --update "$PREVIOUS_TAG" "$NEW_TAG" --file diff_notes.rst
 pandoc --from rst --to markdown_github < diff_notes.rst > diff_notes.md
