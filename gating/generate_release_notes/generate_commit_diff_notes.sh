62496da882225ea8d6661339faaafa70bc8d6ef8
Darren Birkett <darren.birkett@gmail.com>
2017-12-14 10:30:34 +0000

    fix arr path

--- gating/generate_release_notes/generate_commit_diff_notes.sh
+++ gating/generate_release_notes/generate_commit_diff_notes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
-export BASE_DIR="$(readlink -f $(dirname ${0}))"
-source "scripts/functions.sh"
-ln -sf "ansible-role-${RPC_PRODUCT_RELEASE}-requirements.yml" "ansible-role-requirements.yml"
 
-rpc-differ --debug -r "$REPO_URL" --update "$PREVIOUS_TAG" "$NEW_TAG" --file diff_notes.rst
+source "scripts/functions.sh"
+
+rpc-differ --debug -rr "ansible-role-${RPC_PRODUCT_RELEASE}-requirements.yml" -r "$REPO_URL" --update "$PREVIOUS_TAG" "$NEW_TAG" --file diff_notes.rst
 pandoc --from rst --to markdown_github < diff_notes.rst > diff_notes.md
